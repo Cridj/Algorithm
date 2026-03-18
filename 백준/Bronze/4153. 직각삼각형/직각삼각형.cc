09616c8ae771d061b3e5b55d358a1d6fa4fcb5b0
Cridj <95269034+Cridj@users.noreply.github.com>
2026-03-18 18:47:32 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    while (true)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a == 0 && b == 0 && c == 0)
+            break;
+
+        int arr[3] = {a, b, c};
+        sort(arr, arr + 3);
+
+        if (arr[0]*arr[0] + arr[1]*arr[1] == arr[2]*arr[2])
+            cout << "right\n";
+        else
+            cout << "wrong\n";
+    }
+}
