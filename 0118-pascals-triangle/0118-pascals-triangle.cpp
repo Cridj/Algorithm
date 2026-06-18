0aeb02450c829318c67963a3666d15dcd28d912a
Cridj <95269034+Cridj@users.noreply.github.com>
2026-06-18 19:41:19 +0900

    Time: 0 ms (100%), Space: 9.7 MB (30.19%) - LeetHub-Neo

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v(numRows);
+
+        for(int i = 0; i < numRows; i++)
+        {
+            v[i].resize(i + 1);
+            for(int j = 0; j < i + 1; j++)
+            {
+                v[i][j] = 1;
+            }
+        }
+
+        for(int i = 0; i < numRows; i++)
+        {
+            for(int j = 0; j < i + 1; j++)
+            {
+                if(j == 0 || j == i)
+                    continue;
+                v[i][j] = v[i - 1][j] + v[i - 1][j - 1];
+            }
+        }
+        return v;
+    }
+};
